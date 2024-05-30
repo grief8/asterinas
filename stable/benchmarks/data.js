fb9b46faa54af629aa7d671505c8a3058bb2fe8d
github-action-benchmark <github@users.noreply.github.com>
2024-05-30 12:13:27 +0000

    add Sysbench Benchmark (customSmallerIsBetter) benchmark result for c637736446a7c0491d7415269d09ffefd708d36f

---
+++ stable/benchmarks/data.js
@@ -0,0 +1,56 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1717071207776,
+  "repoUrl": "https://github.com/grief8/asterinas",
+  "entries": {
+    "Sysbench Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "lifabing.lfb@antgroup.com",
+            "name": "Fabing Li",
+            "username": "grief8"
+          },
+          "committer": {
+            "email": "lifabing.lfb@antgroup.com",
+            "name": "Fabing Li",
+            "username": "grief8"
+          },
+          "distinct": true,
+          "id": "c637736446a7c0491d7415269d09ffefd708d36f",
+          "message": "test",
+          "timestamp": "2024-05-30T20:03:11+08:00",
+          "tree_id": "b748b595d5291159e0ae07cfb16664cf5181dd24",
+          "url": "https://github.com/grief8/asterinas/commit/c637736446a7c0491d7415269d09ffefd708d36f"
+        },
+        "date": 1717071206832,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Threads Minimum latency",
+            "value": 1510.4,
+            "unit": "ms",
+            "extra": "min"
+          },
+          {
+            "name": "Threads Average Latency",
+            "value": 102160.98,
+            "unit": "ms",
+            "extra": "avg"
+          },
+          {
+            "name": "Threads Maximum Latency",
+            "value": 292707.21,
+            "unit": "ms",
+            "extra": "max"
+          },
+          {
+            "name": "Thread 95th Percentile Latency",
+            "value": 100000,
+            "unit": "ms",
+            "extra": "per95"
+          }
+        ]
+      }
+    ]
+  }
+}
