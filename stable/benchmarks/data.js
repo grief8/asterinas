ad7205b6ca0224477464ac0f2a2688d7b5beff60
github-action-benchmark <github@users.noreply.github.com>
2024-05-31 02:59:42 +0000

    add Sysbench Benchmark (customSmallerIsBetter) benchmark result for c637736446a7c0491d7415269d09ffefd708d36f

--- stable/benchmarks/data.js
+++ stable/benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1717071416138,
+  "lastUpdate": 1717124382036,
   "repoUrl": "https://github.com/grief8/asterinas",
   "entries": {
     "Sysbench Benchmark": [
@@ -40,6 +40,54 @@ window.BENCHMARK_DATA = {
           {
             "name": "Threads Maximum Latency",
             "value": 292707.21,
+            "unit": "ms",
+            "extra": "max"
+          },
+          {
+            "name": "Thread 95th Percentile Latency",
+            "value": 100000,
+            "unit": "ms",
+            "extra": "per95"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "lifabing.lfb@antgroup.com",
+            "name": "Fabing Li",
+            "username": "grief8"
+          },
+          "committer": {
+            "email": "lifabing.lfb@antgroup.com",
+            "name": "Fabing Li",
+            "username": "grief8"
+          },
+          "distinct": true,
+          "id": "c637736446a7c0491d7415269d09ffefd708d36f",
+          "message": "test",
+          "timestamp": "2024-05-30T20:03:11+08:00",
+          "tree_id": "b748b595d5291159e0ae07cfb16664cf5181dd24",
+          "url": "https://github.com/grief8/asterinas/commit/c637736446a7c0491d7415269d09ffefd708d36f"
+        },
+        "date": 1717124381624,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Threads Minimum latency",
+            "value": 1628.86,
+            "unit": "ms",
+            "extra": "min"
+          },
+          {
+            "name": "Threads Average Latency",
+            "value": 109680.24,
+            "unit": "ms",
+            "extra": "avg"
+          },
+          {
+            "name": "Threads Maximum Latency",
+            "value": 299831.87,
             "unit": "ms",
             "extra": "max"
           },
