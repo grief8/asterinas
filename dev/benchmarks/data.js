de0a7d14dd8e1b4f033100ed395746d7fdfc3f9f
github-action-benchmark <github@users.noreply.github.com>
2024-06-20 01:14:04 +0000

    add sysbench Benchmark (customSmallerIsBetter) benchmark result for 8007b07b105fc2d9a4d0fa68f7bd232a71fc75c8

--- dev/benchmarks/data.js
+++ dev/benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1718845529517,
+  "lastUpdate": 1718846044729,
   "repoUrl": "https://github.com/grief8/asterinas",
   "entries": {
     "getpid Benchmark": [
@@ -2698,6 +2698,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Asterinas Threads Average Latency",
             "value": "503.72",
+            "unit": "ms",
+            "extra": "aster_avg"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Fabing Li",
+            "username": "grief8",
+            "email": "lifabing.lfb@antgroup.com"
+          },
+          "committer": {
+            "name": "Fabing Li",
+            "username": "grief8",
+            "email": "lifabing.lfb@antgroup.com"
+          },
+          "id": "8007b07b105fc2d9a4d0fa68f7bd232a71fc75c8",
+          "message": "Merge branch 'labels' into main",
+          "timestamp": "2024-06-19T08:22:46Z",
+          "url": "https://github.com/grief8/asterinas/commit/8007b07b105fc2d9a4d0fa68f7bd232a71fc75c8"
+        },
+        "date": 1718846039016,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Linux Threads Average Latency",
+            "value": "21.20\r",
+            "unit": "ms",
+            "extra": "linux_avg"
+          },
+          {
+            "name": "Asterinas Threads Average Latency",
+            "value": "483.63",
             "unit": "ms",
             "extra": "aster_avg"
           }
