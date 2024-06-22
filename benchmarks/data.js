994ea320595d1ec2a44342394085c472c6c1df7c
github-action-benchmark <github@users.noreply.github.com>
2024-06-22 17:24:12 +0000

    add sysbench-cpu Benchmark (customSmallerIsBetter) benchmark result for 5dc579236fed42ab32c24816984e688682a0bd49

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1719076571076,
+  "lastUpdate": 1719077051992,
   "repoUrl": "https://github.com/grief8/asterinas",
   "entries": {
     "sysbench-thread Benchmark": [
@@ -2450,6 +2450,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Asterinas CPU Average Latency",
             "value": "3.83",
+            "unit": "ms",
+            "extra": "aster_avg"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Fabing Li",
+            "username": "grief8",
+            "email": "lifabing.lfb@antgroup.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "5dc579236fed42ab32c24816984e688682a0bd49",
+          "message": "Update benchmarks.yml",
+          "timestamp": "2024-06-21T14:09:18Z",
+          "url": "https://github.com/grief8/asterinas/commit/5dc579236fed42ab32c24816984e688682a0bd49"
+        },
+        "date": 1719077046197,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Linux CPU Average Latency",
+            "value": "3.83",
+            "unit": "ms",
+            "extra": "linux_avg"
+          },
+          {
+            "name": "Asterinas CPU Average Latency",
+            "value": "3.82",
             "unit": "ms",
             "extra": "aster_avg"
           }
